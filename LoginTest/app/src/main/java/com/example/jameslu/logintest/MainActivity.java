11f65b7d57b724a1c136be417ebfc94ecf535cca
James Lu <Jy7lu@edu.uwaterloo.ca>
2018-01-20 11:24:08 -0500

    Login Test Page

---
+++ LoginTest/app/src/main/java/com/example/jameslu/logintest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.jameslu.logintest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view){
+        Intent intent = new Intent(MainActivity.this, MainPage.class);
+        startActivity(intent);
+    }
+
+
+}
